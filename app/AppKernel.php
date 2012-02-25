971446207e18ce8b8794f9ba2a3a638b3093d4dc
Miguel <miguel@matmor.unam.mx>
2012-02-24 19:00:45 -0600

    Redireccionar logout

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new MakerLabs\PagerBundle\MakerLabsPagerBundle(),
             new FOQ\ElasticaBundle\FOQElasticaBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
