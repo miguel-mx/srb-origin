306077f671ea8fcce7aa0412d6c86ed4aa52d0c4
Miguel <miguel@matmor.unam.mx>
2012-05-02 17:13:07 -0500

    Funcionalidad del catálogo de autores
    	new file:   src/Ccm/SrbBundle/Controller/AuthorController.php
    	modified:   src/Ccm/SrbBundle/Controller/CrudController.php
    	modified:   src/Ccm/SrbBundle/Controller/WebController.php
    	new file:   src/Ccm/SrbBundle/Entity/Author.php
    	new file:   src/Ccm/SrbBundle/Entity/AuthorRepository.php
    	modified:   src/Ccm/SrbBundle/Entity/Referencia.php
    	new file:   src/Ccm/SrbBundle/Form/AuthorType.php
    	new file:   src/Ccm/SrbBundle/Resources/views/Author/edit.html.twig
    	new file:   src/Ccm/SrbBundle/Resources/views/Author/index.html.twig
    	new file:   src/Ccm/SrbBundle/Resources/views/Author/new.html.twig
    	new file:   src/Ccm/SrbBundle/Resources/views/Author/references.html.twig
    	new file:   src/Ccm/SrbBundle/Resources/views/Author/show.html.twig
    	new file:   src/Ccm/SrbBundle/Resources/views/Refs/authors.html.twig
    	modified:   src/Ccm/SrbBundle/Resources/views/Refs/show.html.twig
    	new file:   src/Ccm/SrbBundle/Tests/Controller/AuthorControllerTest.php

--- src/Ccm/SrbBundle/Controller/WebController.php
+++ src/Ccm/SrbBundle/Controller/WebController.php
@@ -121,7 +121,7 @@ class WebController extends BaseController
 
         }
 
-        $addarray[$i]['entryType'] = 'Article';
+        $addarray[$i]['entryType'] = 'Unpublished';
 
         $temp = explode('/abs/',$entry->get_id());
         $addarray[$i]['arxiv'] = $temp[1];
